xiaomi-8550/android_vendor_qcom-opensource_display-commonsys

display: Make UBWC-P opt-out
Enabling by default for new targets. Disable for legacy targets.
This avoids needing to keep defining flags going forward
and means we can drop these patches when existing devices
age out.

Change-Id: I2cbfa3fe6a9f1c5522286d27aab713027e743bc9

config/display-product-commonsys.mk
@@ -8,7 +8,7 @@ PRODUCT_PACKAGES += libdisplayconfig.system \
 
 SOONG_CONFIG_NAMESPACES += qtidisplaycommonsys
 # Soong Keys
-SOONG_CONFIG_qtidisplaycommonsys := displayextension composer3ext gralloc_handle_has_no_reserved_size gralloc_handle_has_no_custom_content_md_reserved_size
+SOONG_CONFIG_qtidisplaycommonsys := displayextension composer3ext gralloc_handle_has_no_reserved_size gralloc_handle_has_no_custom_content_md_reserved_size gralloc_handle_has_no_ubwcp
 # Soong Values
 
 # displayextension controls global compile time disablement of SF extensions
@@ -25,6 +25,8 @@ SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_reserved_size := false
 
 SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_custom_content_md_reserved_size := false
 
+SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_ubwcp := false
+
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     SOONG_CONFIG_qtidisplaycommonsys_displayextension := true
     SOONG_CONFIG_qtidisplaycommonsys_composer3ext := true
@@ -36,4 +38,8 @@ endif
 
 ifeq ($(TARGET_GRALLOC_HANDLE_HAS_NO_CUSTOM_CONTENT_MD_RESERVED_SIZE),true)
     SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_custom_content_md_reserved_size := true
+endif
+
+ifeq ($(TARGET_GRALLOC_HANDLE_HAS_NO_UBWCP),true)
+    SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_ubwcp := true
 endif

aswin7469/vendor_qcom_opensource_commonsys_display

Signed-off-by: aswin7469 <aswinas@pixysos.com>

config/display-product-commonsys.mk
@@ -8,7 +8,7 @@ PRODUCT_PACKAGES += libdisplayconfig.system \
 
 SOONG_CONFIG_NAMESPACES += qtidisplaycommonsys
 # Soong Keys
-SOONG_CONFIG_qtidisplaycommonsys := gralloc_handle_has_no_reserved_size displayextension composer3ext gralloc_handle_has_no_custom_content_md_reserved_size
+SOONG_CONFIG_qtidisplaycommonsys := gralloc_handle_has_no_reserved_size displayextension composer3ext gralloc_handle_has_no_custom_content_md_reserved_size gralloc_handle_has_no_ubwcp
 # Soong Values
 SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_reserved_size := false
 SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_custom_content_md_reserved_size := false
@@ -21,6 +21,8 @@ SOONG_CONFIG_qtidisplaycommonsys_displayextension := false
 # and the feature can be enabled/disabled at run time via android
 # properties
 SOONG_CONFIG_qtidisplaycommonsys_composer3ext := false
+SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_ubwcp := false
+
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     SOONG_CONFIG_qtidisplaycommonsys_displayextension := true
@@ -33,4 +35,8 @@ endif
 
 ifeq ($(TARGET_GRALLOC_HANDLE_HAS_NO_CUSTOM_CONTENT_MD_RESERVED_SIZE),true)
     SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_custom_content_md_reserved_size := true
+endif
+
+ifeq ($(TARGET_GRALLOC_HANDLE_HAS_NO_UBWCP),true)
+    SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_ubwcp := true
 endif